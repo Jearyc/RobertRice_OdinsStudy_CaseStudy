ac7636958e68fd37ab45e1a837435718e322721b
rjric <rjrice_94@yahoo.com>
2021-02-17 14:33:15 -0500

    First Commit

---
+++ src/main/java/com/jearyc/odinsstudy/RobertRiceOdinsStudyCaseStudyApplication.java
@@ -0,0 +1,13 @@
+package com.jearyc.odinsstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobertRiceOdinsStudyCaseStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobertRiceOdinsStudyCaseStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jearyc/odinsstudy/RobertRiceOdinsStudyCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jearyc.odinsstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobertRiceOdinsStudyCaseStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
